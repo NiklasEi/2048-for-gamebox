62852c78878e9886bc342bfaa9ef577f3ae017ab
Niklas Eicker <nikle1@gmx.de>
2017-05-12 14:38:41 +0200

    create directory if default english file is missing

--- src/me/nikl/twoofoureight/Language.java
+++ src/me/nikl/twoofoureight/Language.java
@@ -83,6 +83,7 @@ public class Language {
 		
 		File defaultEn = new File(plugin.getDataFolder().toString() + File.separatorChar + "language" + File.separatorChar + "lang_en.yml");
 		if(!defaultEn.exists()){
+			defaultEn.getParentFile().mkdirs();
 			plugin.saveResource("language" + File.separatorChar + "lang_en.yml", false);
 		}
 		File defaultDe = new File(plugin.getDataFolder().toString() + File.separatorChar + "language" + File.separatorChar + "lang_de.yml");
