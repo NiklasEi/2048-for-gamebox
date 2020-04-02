11b23384d9416f7bb07f6d125f0bcff10cd7a676
Niklas Eicker <git@nikl.me>
2020-04-02 22:51:27 +0200

    Update to GameBox v3.0.0-beta.2

--- src/main/java/me/nikl/gamebox/games/TofeMain.java
+++ src/main/java/me/nikl/gamebox/games/TofeMain.java
@@ -1,31 +1,23 @@
 package me.nikl.gamebox.games;
 
-import me.nikl.gamebox.GameBox;
-import me.nikl.gamebox.Module;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
+import me.nikl.gamebox.games.twoofoureight.Tofe;
+import me.nikl.gamebox.module.GameBoxModule;
 
 /**
  * Created by Niklas on 14.04.2017.
  *
  * TofeMain class for the GameBox game 2048
  */
-public class TofeMain extends JavaPlugin {
-    private GameBox gameBox;
+public class TofeMain extends GameBoxModule {
     public static final String TWO_O_FOUR_EIGHT = "twoofoureight";
 
     @Override
     public void onEnable() {
-        Plugin plugin = Bukkit.getPluginManager().getPlugin("GameBox");
-        if(plugin == null || !plugin.isEnabled()){
-            getLogger().warning(" GameBox was not found! Disabling LogicPuzzles...");
-            Bukkit.getPluginManager().disablePlugin(this);
-            return;
-        }
-        gameBox = (GameBox) plugin;
-        new Module(gameBox, TWO_O_FOUR_EIGHT
-                , "me.nikl.gamebox.games.twoofoureight.Tofe"
-                , this, TWO_O_FOUR_EIGHT, "2048", "tofe");
+        registerGame(TWO_O_FOUR_EIGHT, Tofe.class, "tofe");
+    }
+
+    @Override
+    public void onDisable() {
+
     }
 }
